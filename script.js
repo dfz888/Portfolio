e8f11fdf034a145614fe56254e9224c5869f5614
dfz888 <david.ferrer@csic.es>
2025-03-14 12:59:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const sections = document.querySelectorAll("section");
-    sections.forEach((section) => {
-        section.classList.add("visible");
-    });
-});
-
 function toggleDetails(id) {
     const content = document.getElementById(id);
-    content.style.display = (content.style.display === "none" || content.style.display === "") ? "block" : "none";
+    content.style.display = content.style.display === "none" || content.style.display === "" ? "block" : "none";
 }
+
